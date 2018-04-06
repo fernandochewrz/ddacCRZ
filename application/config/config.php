13b4e2425f28b505d5aef738091fb2ddf28d40bd
fernandochewrz <38097503+fernandochewrz@users.noreply.github.com>
2018-04-06 18:48:27 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ddacCRZ/';
+$config['base_url'] = 'https://ddactp038150.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
